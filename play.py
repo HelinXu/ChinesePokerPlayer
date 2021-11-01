b5c08f9b6d4aa572802e3e2e843ae795351206e0
HelinXu <xhl19@mails.tsinghua.edu.cn>
2021-11-01 15:57:13 +0800

    update display

--- play.py
+++ play.py
@@ -14,13 +14,15 @@ if os.path.isfile(path):
 
 class MainWindow(object):
     _minWidth, _minHeight = 1280, 720
-    card_imgs = []
+    _marginX, _marginY, _marginC = 30, 30, 20
+    _cardX, _cardY = 100, 140
+
     def __init__(self):
         self.window = tk.Tk()
         self.window.title("Chinese Poker")
         self.window.minsize(self._minWidth, self._minHeight)
         self.cards = np.array([0] * 15)
-        # self.card_imgs = []
+        self.card_imgs = []
         self.init_card_display()
         self.window.mainloop()
 
@@ -32,10 +34,13 @@ class MainWindow(object):
         self.canvas.place(relx=0, rely=0)
         
         for i in range(54):
-            self.card_imgs.append(ImageTk.PhotoImage(Image.open(f'resource/pokers/{i+1}.png')))
-            self.canvas.create_image((30 + 21*i, 30), anchor='nw', image=self.card_imgs[i])
+            self.card_imgs.append(ImageTk.PhotoImage(
+                    (Image.open(f'resource/pokers/{i+1}.png')).resize((self._cardX, self._cardY))
+                    ))
+            self.canvas.create_image((self._marginX + 21*i, self._marginY), anchor='nw', image=self.card_imgs[i])
         print('success initial display.')
 
+    # def 
 
 if __name__ == '__main__':
     MainWindow()
